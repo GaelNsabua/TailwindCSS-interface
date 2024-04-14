3cce9b594c6df5beae125d10204593bc9c95a109
Gael Mukala <gaelmukala14@gmail.com>
2024-04-15 00:56:24 +0100

    Modifications on nav

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 // This helps to run the function as soon as the document is loaded
 $(function(){
+    // Here we make menu visible or hidden when the user clicks on the menu icon. This is for mobile device 
     let menuBtn = $("#menu-btn")
     let menu = $("#menu")
 
@@ -8,4 +9,18 @@ $(function(){
         menu.toggleClass("hidden")
     })
 
+    // Here we handle the menu tags selection 
+    // This line helps to get all menu tags
+    let allMenuTags = $(".menu-tag")
+    // Here we listen the click on a tag
+    allMenuTags.on("click", function(){
+        // Here we remove the class active to the current element
+        allMenuTags.each(function(){
+            $(this).removeClass("active")
+        })
+        // Here we add the class active to the element the user has clicked on 
+        $(this).addClass("active")
+    })
+    
 })
+
